5ed6b56424b31364b0ce4c533e0f97ca2d7fc39b
Joseph Harris <josephharris@Josephs-MacBook-Pro.local>
2023-03-03 13:20:47 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function testClickOne () {
+    alert('fuck')
+}
+
+function testClickTwo () {
+    document.querySelector('.headingOneText').innerText = "You just changed the Heading text";
+}
+p];,,,,,,,,
+// example of console logging
+console.log('output: ', document.getElementById('Button'))
